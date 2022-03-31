47b9ac937389445f7a128ba1851c1e6748dc2e09
Gustavo Inacio <incio.gusta@gmail.com>
2022-03-31 02:12:02 -0300

    add useInput and useSelect hooks

---
+++ libs/hooks/useSelect.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export type BindSelect = {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+
+export function useSelect(initialValue: string): BindSelect {
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setValue(event.target.value);
+  };
+
+  return { value, onChange };
+}
